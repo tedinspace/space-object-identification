73b5c31a63986eccb62ac1b3f2acc02d3ff0fc72
ted <tmcculloch@g.harvard.edu>
2025-03-16 08:17:32 -0400

    put timestamp manipulation into time utils

--- src/utils/time.py
+++ src/utils/time.py
@@ -1,6 +1,14 @@
 
 from datetime import datetime
 
-def timestamp_file():
-    '''creates a timestamp string (ISO)'''
-    return datetime.now().isoformat().replace('-','_').replace(':','_').split('.')[0]
+def timestamp_file_now():
+    '''iso now file timestamp'''
+    return timestampe_file(datetime.now())
+
+def timestampe_file(datetime_obj):
+    '''make a timestamp string that can be put into a file name'''
+    return datetime_obj.isoformat().replace('-','_').replace(':','_').split('.')[0]
+
+def timestamp_query(datetime_object):
+    '''YYYY-MM-DD iso string'''
+    return datetime_object.strftime('%Y-%m-%d')
