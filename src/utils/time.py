1934f73bcf992526c3ab6074501cea8950e78363
ted <tmcculloch@g.harvard.edu>
2025-04-21 20:29:43 -0400

    eda related to epochs

--- src/utils/time.py
+++ src/utils/time.py
@@ -1,5 +1,5 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def timestamp_file_now():
     '''iso now file timestamp'''
@@ -12,3 +12,10 @@ def timestampe_file(datetime_obj):
 def timestamp_query(datetime_object):
     '''YYYY-MM-DD iso string'''
     return datetime_object.strftime('%Y-%m-%d')
+
+
+def year_doy_to_datetime(epoch):
+    year = int(epoch // 1000)
+    day_of_year = epoch % 1000
+    year += 2000 if year < 57 else 1900
+    return  datetime(year, 1, 1) + timedelta(days=day_of_year - 1)
